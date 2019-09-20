2ab15a8361c10aa87582c4cc3095e0e00556059e
Mike Cooper <mikescooper@icloud.com>
2019-09-20 00:57:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,13 @@ class Student < User
   def initialize
     @knowledge = []
   end 
+  
+  def knowledge
+    @knowledge
+  end 
+  
+  def learn(string_of_knowledge)
+    self.knowledge << string_of_knowledge
+  end 
 
 end
