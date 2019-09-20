93f271ff3127feafe535566b9825dd4ce0bdcbb3
Mike Cooper <mikescooper@icloud.com>
2019-09-20 00:57:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+  
+  def initialize
+    @knowledge = []
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
+  
+  def learn(string_of_knowledge)
+    self.knowledge << string_of_knowledge
+  end 
 
 end
